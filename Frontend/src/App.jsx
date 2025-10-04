85926d0e528726522e551d7ea66c21c79d29c2e8
arya patel <aryaunjha@gmail.com>
2025-10-04 10:10:32 +0530

    Add appRoutes in app.jsx and add context in main.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loginUser } from "./Redux/Slice";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const token = localStorage.getItem("token");
+    if (user && token) {
+      dispatch(loginUser({ user, token }));
+    }
+  }, [dispatch]);
 
   return (
-    <>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
