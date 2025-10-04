650eeaaf6a157ba3c2e73e80949a9d79bd934a9f
Sujal Vasara <vasarasujal.cg@gmail.com>
2025-10-04 12:39:26 +0530

    Add Layout for manager dashboard

---
+++ Frontend/src/Components/Layout/ManagerLayout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "../companySidebar";
+import Header from "../Header.jsx";
+import { Outlet } from "react-router-dom";
+
+const HrLayoutDashboard = () => {
+    return (
+        <div className="min-h-screen flex flex-col bg-gray-1">
+            <Header />
+            <div className="flex flex-1">
+                <Sidebar />
+                <main className="flex-1 overflow-y-auto">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+};
+export default HrLayoutDashboard;
